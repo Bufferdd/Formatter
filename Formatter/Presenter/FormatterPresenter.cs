e2d8c15d46ed7d58310ae5e544142267bae7201f
AlexKovat <kovatskii888@mail.ru>
2023-10-08 17:23:42 +0700

    Fix initial FormatterPresenter

--- Formatter/Presenter/FormatterPresenter.cs
+++ Formatter/Presenter/FormatterPresenter.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Formatter.Model;
+using Formatter.View;
+
 namespace Formatter.Presenter
 {
     class FormatterPresenter
     {
+        private IModel _model;
+        private IView _view;
 
+        public FormatterPresenter(IView view, IModel model)
+        {
+            _view = view;
+            _model = new FormatterModel(null);
+        }
     }
 }
