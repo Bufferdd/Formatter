315d120b9dc2ae2861f9f98f2489c71b95c5cb24
Gus13031989 <p61827624@gmail.com>
2023-10-09 18:08:58 +0700

    add unit test for SaveInFile

---
+++ FormatterTests/FormatterModelTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+using Formatter.Model;
+
+namespace FormatterTests
+{
+    [TestClass]
+    public class FormatterModelTests
+    {
+        [TestMethod]
+        public void Test_SaveInFile()
+        {
+            string text = "Тестовый текст для проверки модели";
+            string filename = "Тест";
+            string format = ".frmt";
+
+            FormatterModel model = new FormatterModel(text);
+            IModel iModel = model;
+
+            iModel.SaveInFile(filename, format);
+
+            Assert.AreEqual(true, File.Exists(filename + format));
+
+            using (StreamReader reader = new StreamReader(filename + format)) 
+            {
+                Assert.AreEqual(text, reader.ReadToEnd());
+            }
+
+            File.Delete(filename + format);
+        }
+    }
+}
